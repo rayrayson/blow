aa605b0c7b43e4ecc8ee5008388c1099098b8977
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2012-09-01 18:22:01 +0200

    Fix typos in doc

--- src/main/core/blow/Project.groovy
+++ src/main/core/blow/Project.groovy
@@ -7,6 +7,6 @@ package blow
 final class Project {
            final static def name = "blow"
            final static def number = "0.7.0"
-           final static def version = "0.7.0_1346489360552"
-           final static def timestamp = new Date(1346489360552)
+           final static def version = "0.7.0_1346497549949"
+           final static def timestamp = new Date(1346497549949)
 }
